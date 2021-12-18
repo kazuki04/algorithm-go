39ac48bdf4bfd7636701ebc235b50b5e9c8e9175
kazuki04 <kazukik0402@gmail.com>
2021-12-18 21:00:34 +0900

    Add struct Node and DoublyLinkedList

---
+++ doubly_linked_list/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data          int
+	previous_node *Node
+	next_node     *Node
+}
+
+type DoublyLinkedList struct {
+	head *Node
+}
+
+func main() {
+
+}
