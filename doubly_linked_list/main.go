0d848656997de82441b43b215bd1e14ec7eae3ce
kazuki04 <kazukik0402@gmail.com>
2021-12-18 21:10:13 +0900

    Define append method

--- doubly_linked_list/main.go
+++ doubly_linked_list/main.go
@@ -12,6 +12,24 @@ type DoublyLinkedList struct {
 	head *Node
 }
 
-func main() {
+func (d *DoublyLinkedList) append(data int) {
+	new_node := &Node{data: data}
+	if d.head == nil {
+		d.head = new_node
+		return
+	}
+	current_node := d.head
+	for current_node.next_node != nil {
+		current_node = current_node.next_node
+	}
+	current_node.next_node = new_node
+	new_node.previous_node = current_node
+}
 
+func main() {
+	d := &DoublyLinkedList{nil}
+	d.append(1)
+	d.append(2)
+	fmt.Println(d.head.data)
+	fmt.Println(d.head.next_node.data)
 }
