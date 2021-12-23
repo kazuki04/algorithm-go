c1591bfb6e7582a3efe04045005fae9fa8d1a4a9
kazuki04 <kazukik0402@gmail.com>
2021-12-23 23:27:59 +0900

    Add newHashTable and add methods

--- hash_table/main.go
+++ hash_table/main.go
@@ -7,17 +7,39 @@ import (
 )
 
 type HashTable struct {
-	size  int
-	table [][]interface{}
+	size int64
+	// To DO : What is the differences between [][]interface and [][]map[interface{}]intreface{}
+	// table [][]interface{}
+	table [][]map[interface{}]interface{}
 }
 
-func (h *HashTable) hash(key string, size int64) int64 {
+func newHashTable(size int64) *HashTable {
+	h := &HashTable{size: size, table: make([][]map[interface{}]interface{}, size)}
+	return h
+}
+
+// To Do: Consider the type of key. string or interface{}
+func (h *HashTable) hash(key string) int64 {
 	sum := sha256.Sum256([]byte(key))
 	parsed_sum_base_16, _ := strconv.ParseInt(fmt.Sprintf("%x", sum), 16, 64)
-	return parsed_sum_base_16 % size
+	return parsed_sum_base_16 % h.size
+}
+
+func (h *HashTable) add(key string, value interface{}) {
+	index := h.hash(key)
+
+	for _, data := range h.table[index] {
+		if data[0] == key {
+			data[1] = value
+			return
+		}
+	}
+	element := make(map[interface{}]interface{})
+	element[key] = value
+	h.table[index] = append(h.table[index], element)
 }
 
 func main() {
-	h := &HashTable{size: 10}
-	fmt.Println(h.hash("test", 10))
+	h := newHashTable(10)
+	fmt.Println(h)
 }
