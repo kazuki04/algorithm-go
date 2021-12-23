2e77c1ef87cdcdbaf2f736e66fb35a2693eb550f
kazuki04 <kazukik0402@gmail.com>
2021-12-23 19:12:45 +0900

    Add hash method

--- hash_table/main.go
+++ hash_table/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"strconv"
 )
 
 type HashTable struct {
@@ -10,10 +11,13 @@ type HashTable struct {
 	table [][]interface{}
 }
 
-func (h *HashTable) hash(key string) string {
-	sum := sha256.Sum256([]byte("hello world"))
-	return fmt.Sprintf("%x", sum)
+func (h *HashTable) hash(key string, size int64) int64 {
+	sum := sha256.Sum256([]byte(key))
+	parsed_sum_base_16, _ := strconv.ParseInt(fmt.Sprintf("%x", sum), 16, 64)
+	return parsed_sum_base_16 % size
 }
 
 func main() {
+	h := &HashTable{size: 10}
+	fmt.Println(h.hash("test", 10))
 }
