9c4d8fdd30a4728fe1c06fa2be6be47b3d2e86f4
kazuki04 <kazukik0402@gmail.com>
2022-01-18 21:16:36 +0900

    Modify push method and stack type

--- stack/main.go
+++ stack/main.go
@@ -2,20 +2,16 @@ package main
 
 import "fmt"
 
-type Stack struct {
-	stack *[]int
-}
+type stack []int
 
-func (s *Stack) push(data int) {
-	var stack = s.stack
+func (stack *stack) push(data int) {
 	*stack = append(*stack, data)
 }
 
 func main() {
-	stack := make([]int, 0)
-	s := Stack{&stack}
+	var s stack = make([]int, 0)
 	s.push(1)
 	s.push(2)
 	s.push(3)
-	fmt.Println(stack)
+	fmt.Println(s)
 }
